9d6ced995deca4e49d0e9fe825b6c485110bd57b
italo-oliveira <140121312+italo1106@users.noreply.github.com>
2023-10-18 17:21:40 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+<link rel="stylesheet" href="exp01.css">
+</head>
+<body>
+    <pre>seletores</pre>
+    <h1>titulo</h1>
+    <h2 >subtitulo</h2>
+    <p>paragrafo</p>
+    <p >paragrafo</p>
+    <pre>seletores com o uso de id = para espescificar um unico elemento</pre>
+    <p id="stilo1">paragrafo</p>
+    <p>paragrafo</p>
+    
+    <p>paragrafo</p>
+    <pre>seletores com o uso de class = para varios elementos</pre>
+   <p. class="stilo2">usa-me<br></p.>
+
+   <p. class="stilo2">paragrafo<br></p.>
+
+   <p. class="stilo2">paragrafo<br></p.>
+
+   <p. class="stilo2">paragrafo<br></p.>
+
+
+
+
+
+    <?php
+    
+    ?>
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp02</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+    
+<h1>titulo</h1>
+
+<p class="stilo">classe 01</p>
+
+<p>p de fora</p>
+
+<div class="conteudo">
+
+<p>titulo da div</p>
+
+<p id="titulo">dentro do subcontexto</p>
+
+</div>
+
+</div>
+
+
+</body>
+</html>
