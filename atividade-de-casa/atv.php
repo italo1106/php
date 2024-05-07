d331058cb7f3b9de3caeee02e3009b328444b3cb
italo-oliveira <140121312+italo1106@users.noreply.github.com>
2024-05-07 14:19:01 -0300

    Add files via upload

---
+++ atividade-de-casa/atv.php
@@ -0,0 +1,137 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$aluno["nome"] = "cleber";
+$aluno["endereco"] = "rua da casa da mae joana";
+$aluno["cidade"] = "cocos";
+$aluno["estado"] = "bahia";
+$aluno["idade"] = "26";
+$aluno["cpf"] = "455.915.925-85";
+
+echo $aluno["nome"];
+echo "<br>";
+echo $aluno["cidade"];
+echo "<br>";
+echo $aluno["estado"];
+echo "<br>";
+echo $aluno["idade"];
+echo "<br>";
+echo $aluno["cpf"];
+echo "<br>";
+echo "<br>";
+
+$aluno1["nome"] = "gabriel";
+$aluno1["endereco"] = "avenida dummont";
+$aluno1["cidade"] = "Guanambi";
+$aluno1["estado"] = "bahia";
+$aluno1["idade"] = "47";
+$aluno1["cpf"] = "857.996.754-47";
+
+echo $aluno1["nome"];
+echo "<br>";
+echo $aluno1["endereco"];
+echo "<br>";
+echo $aluno1["cidade"];
+echo "<br>";
+echo $aluno1["estado"];
+echo "<br>";
+echo $aluno1["idade"];
+echo "<br>";
+echo $aluno1["cpf"];
+echo "<br>";
+echo "<br>";
+
+$aluno2["nome"] = "davi";
+$aluno2["endereco"] = "rua carlos milton frota";
+$aluno2["cidade"] = "iuiu";
+$aluno2["estado"] = "Bahia";
+$aluno2["idade"] = "17";
+$aluno2["cpf"] = "666.989.321-98";
+
+echo $aluno2["nome"];
+echo "<br>";
+echo $aluno2["endereco"];
+echo "<br>";
+echo $aluno2["cidade"];
+echo "<br>";
+echo $aluno2["estado"]; 
+echo "<br>";
+echo $aluno2["idade"]; 
+echo "<br>";
+echo $aluno2["cpf"];
+echo  "<br>";
+echo "<br>";
+
+$professor["nome"] = "Weliton";
+$professor["endereco"] = "condominio francisco";
+$professor["cidade"] = "Guanambi";
+$professor["estado"] = "bahia";
+$professor["idade"] = "60";
+$professor["materia"] = "P.I";
+
+echo $professor["nome"];
+echo "<br>";
+echo $professor["endereco"];
+echo "<br>";
+echo $professor["cidade"];
+echo "<br>";
+echo $professor["estado"];
+echo "<br>";
+echo $professor["idade"];
+echo "<br>";
+echo $professor["materia"];
+echo "<br>";
+echo "<br>";
+
+$professor1["nome"] = "Romulo";
+$professor1["endereco"] = "avenida santos dumont";
+$professor1["cidade"] = "Ouro Preto";
+$professor1["estado"] = "Rio Grande do Sul";
+$professor1["idade"] = "35";
+$professor1["materia"] = "Logica de Programaçao";
+
+echo $professor1["nome"];
+echo "<br>";
+echo $professor1["endereco"];
+echo "<br>";
+echo $professor1["cidade"];
+echo "<br>";
+echo $professor1["estado"];
+echo "<br>";
+echo $professor1["idade"];
+echo "<br>";
+echo $professor1["materia"];
+echo "<br>";
+echo "<br>";
+
+$professor2["nome"] = "Bruno andrade";
+$professor2["endereco"] = "salvador";
+$professor2["cidade"] = "Guanambi";
+$professor2["estado"] = "Bahia";
+$professor2["idade"] = "32";
+$professor2["materia"] = "geografia";
+
+echo $professor2["nome"];
+echo "<br>";
+echo $professor2["endereco"];
+echo "<br>";
+echo $professor2["cidade"];
+echo "<br>";
+echo $professor2["estado"];
+echo "<br>";
+echo $professor2["idade"];
+echo "<br>";
+echo $professor2["materia"];
+echo "<br>";
+echo "<br>";
+
+?>
+</body>
+</html>
